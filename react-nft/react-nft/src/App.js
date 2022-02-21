3b270a6c1b4be5d215bc0e8aa6c1e979a362ba12
Nguyễn Văn Dương <duongbkak55@gmail.com>
2022-02-21 05:19:17 +0000

    react nft example

---
+++ react-nft/react-nft/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="flex flex-row">
+      <div class="basis-1/4 bg-amber-500 m-2">01</div>
+      <div class="basis-1/4 bg-amber-500 m-2">02</div>
+      <div class="basis-1/2 bg-amber-500 m-2">03</div>
+    </div>
+
+  );
+}
+
+export default App;
