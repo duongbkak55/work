2c849de56f8431703cacaeefc6e33c895b9d3e73
Nguyễn Văn Dương <duongbkak55@gmail.com>
2022-02-22 00:40:15 +0000

    week1

--- react-nft/react-nft/src/App.js
+++ react-nft/react-nft/src/App.js
@@ -3,10 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div class="flex flex-row">
-      <div class="basis-1/4 bg-amber-500 m-2">01</div>
-      <div class="basis-1/4 bg-amber-500 m-2">02</div>
-      <div class="basis-1/2 bg-amber-500 m-2">03</div>
+    <div>
+      <div class="flex flex-row items-center justify-between">
+        <div class="p-3 basis-1/12 bg-amber-500 m-2">01</div>
+        <div class="basis-1/6 bg-amber-500 m-2 text-center">02</div>
+        <div class="basis-1/6 bg-amber-500 m-2">03</div>
+
+      </div>
+      <div class="grid justify-items-start ">
+        <div class="basis-1/3 bg-amber-500">01</div>
+        <div class="basis-1/3 bg-amber-500">02</div>
+        <div class="basis-1/3 bg-amber-500">03</div>
+        <div class="basis-1/3 bg-amber-500">04</div>
+        <div class="basis-1/3 bg-amber-500">05</div>
+        <div class="basis-1/3 bg-amber-500">06</div>
+      </div>
+      <div class="grid justify-items-center ">
+        <div class="basis-1/3 bg-amber-500">01</div>
+        <div class="basis-1/3 bg-amber-500">02</div>
+        <div class="basis-1/3 bg-amber-500">03</div>
+        <div class="basis-1/3 bg-amber-500">04</div>
+        <div class="basis-1/3 bg-amber-500">05</div>
+        <div class="basis-1/3 bg-amber-500">06</div>
+      </div>
+      <div class="grid justify-items-end ">
+      <div class="basis-1/3 bg-amber-500">01</div>
+        <div class="basis-1/3 bg-amber-500">02</div>
+        <div class="basis-1/3 bg-amber-500">03</div>
+        <div class="basis-1/3 bg-amber-500">04</div>
+        <div class="basis-1/3 bg-amber-500">05</div>
+        <div class="basis-1/3 bg-amber-500">06</div>
+      </div>
     </div>
 
   );
